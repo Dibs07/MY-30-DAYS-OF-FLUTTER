38cf4981b0a60f9bdb7f0cf9df140dc0c933fb58
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2023-06-21 20:37:51 +0530

    Day 4 completed

--- lib/Screens/login_page.dart
+++ lib/Screens/login_page.dart
@@ -1,19 +1,55 @@
 import 'package:flutter/material.dart';
 
 class Loginpage extends StatelessWidget {
-  const Loginpage({super.key});
+  //const Loginpage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text(
-          "Login page",
-          style: TextStyle(
-            fontSize: 35,
-            color: Colors.green,
-            fontWeight: FontWeight.bold,
-          ),
+    return Material(
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 35.0),
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/login_image.jpg",
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: "Enter Username",
+                labelText: "Username",
+              ),
+            ),
+            TextFormField(
+              obscureText: false,
+              decoration: const InputDecoration(
+                hintText: "Enter Password",
+                labelText: "Password",
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              child: const Text("Login"),
+              style: TextButton.styleFrom(),
+              onPressed: () {},
+            ),
+          ],
         ),
       ),
     );
