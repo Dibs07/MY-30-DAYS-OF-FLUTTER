92c0637e50fdc09d082d25df46db9f653d9ab500
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2023-06-21 20:38:40 +0530

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/Screens/home_page.dart';
 import 'package:flutterapp/Screens/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,12 +18,14 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         primaryColor: Colors.blue,
+        primaryTextTheme: GoogleFonts.latoTextTheme(),
+        fontFamily: GoogleFonts.lato().fontFamily,
       ),
-      initialRoute: "/Home",
+      initialRoute: "/",
       routes: {
-        "/": (context) => const Loginpage(),
+        "/": (context) => Loginpage(),
         "/Home": (context) => const Homepage(),
-        "/Login": (context) => const Loginpage(),
+        "/Login": (context) => Loginpage(),
       },
     );
   }
