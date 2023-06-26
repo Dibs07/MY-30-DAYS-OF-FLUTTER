6a8a1e01a9be0de09ff3c5c3e55b4057034dd4c9
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2023-06-26 16:55:29 +0530

    Day 12 completed

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutterapp/models/catalog.dart';
-
+import 'dart:convert';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
   final count = 1000;
+  @override
+  void initState() {
+    super.initState();
+    loaddata();
+  }
+
+  loaddata() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummylist = List.generate(20, (index) => CatalogModel.items[0]);
--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -4,7 +4,8 @@ import '../models/catalog.dart';
 
 class ItemWidget extends StatelessWidget {
   final Item item;
-  const ItemWidget({super.key, required this.item}) : assert(item != null);
+  const ItemWidget({super.key, required this.item});
+  //: assert(item != null);
   @override
   Widget build(BuildContext context) {
     return Card(
