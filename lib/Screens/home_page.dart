c0005631b9a588a22528ae0d66c1bfc0f3ff18fc
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2023-06-26 16:21:01 +0530

    day 11 completed

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutterapp/models/catalog.dart';
 
 import '../widgets/drawer.dart';
+import '../widgets/item_widget.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
   final count = 1000;
   @override
   Widget build(BuildContext context) {
+    final dummylist = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         //backgroundColor: Colors.white,
@@ -14,14 +17,22 @@ class Homepage extends StatelessWidget {
         iconTheme: const IconThemeData(color: Colors.black),
         title: const Text(
           "MyCatalog",
-          //style: TextStyle(
-          //color: Colors.black,
-          // ),
+          //style: TextStyle(color: Colors.black, ),
         ),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text("I love flutter $count"),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: ListView.builder(
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              //item: CatalogModel.items[index],
+              item: dummylist[index],
+            );
+          },
+          //itemCount: CatalogModel.items.length,
+          itemCount: dummylist.length,
+        ),
       ),
       drawer: const MyDrawer(),
     );
